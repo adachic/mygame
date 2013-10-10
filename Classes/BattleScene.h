e79899dbf7ddbd67c78482bd7d260e8cb28080a5
adachi_c <adachi.c.akinori@gmail.com>
2013-10-10 23:34:10 +0900

    Added removeCookie

--- Classes/BattleScene.h
+++ Classes/BattleScene.h
@@ -11,8 +11,8 @@
 #include "Config.h"
 
 #define PNG_BACKGROUND "white_bg.png"
-#define MAX_BLOCK_X 13
-#define MAX_BLOCK_Y 13
+#define MAX_COOKIE_X 7
+#define MAX_COOKIE_Y 7
 
 class BattleScene : public cocos2d::CCLayer {
 protected:
@@ -39,12 +39,21 @@ protected:
 
     int getTag(int posIndexX, int posIndexY);
 
+    void getTouchCookieTag(cocos2d::CCPoint touchPoint, int &tag, kCookie &cookieType);
+    std::list<int> getSameColorCookieTags(int baseTag, kCookie cookieType);
+    void removeCookie(std::list<int> cookieTags, kCookie cookieType);
+    bool hasSameColorCookie(std::list<int> cookieTagList, int searchCookieTag);
+
 public:
     virtual bool init();
 
     static cocos2d::CCScene *scene();
 
     CREATE_FUNC(BattleScene);
+
+    virtual bool ccTouchBegan(cocos2d::CCTouch* pTouch, cocos2d::CCEvent* pEvent);
+    virtual void ccTouchMoved(cocos2d::CCTouch* pTouch, cocos2d::CCEvent* pEvent);
+    virtual void ccTouchEnded(cocos2d::CCTouch* pTouch, cocos2d::CCEvent* pEvent);
 };
 
 #endif //__GameScene_H_
--- Classes/BattleScene.mm
+++ Classes/BattleScene.mm
@@ -25,6 +25,9 @@ bool BattleScene::init() {
         return false;
     }
 
+    setTouchEnabled(true);
+    setTouchMode(kCCTouchesOneByOne);
+
     initForVariables();
 
     showBackground();
@@ -63,19 +66,103 @@ int BattleScene::getTag(int posIndexX, int posIndexY) {
     return kTagBaseCookie + posIndexX * 100 + posIndexY;
 }
 
-//プレイヤー表示
+//クッキー表示
 void BattleScene::showCookies() {
-    for (int x = 0; x < MAX_BLOCK_X; x++) {
-        for (int y = 0; y < MAX_BLOCK_Y; y++) {
-            kCookie cookieType = (kCookie)(rand() % 5);
-            int tag = getTag(x,y);
+    for (int x = 0; x < MAX_COOKIE_X; x++) {
+        for (int y = 0; y < MAX_COOKIE_Y; y++) {
+            kCookie cookieType = (kCookie) (rand() % 5);
+            int tag = getTag(x, y);
             m_cookieTags[cookieType].push_back(tag);
 
-            CookieSprite* cp = CookieSprite::createWithCookieType(cookieType);
-            cp->setPosition(getPosition(x,y));
-            m_background->addChild(cp,kZOrderCookie, tag);
+            CookieSprite *cp = CookieSprite::createWithCookieType(cookieType);
+            cp->setPosition(getPosition(x, y));
+            m_background->addChild(cp, kZOrderCookie, tag);
         }
     }
 }
 
+//タッチされたクッキーのタグとタイプを得る
+void BattleScene::getTouchCookieTag(CCPoint touchPoint, int &tag, kCookie &cookieType) {
+    for (int x = 0; x < MAX_COOKIE_X ; x++) {
+        for (int y = 0; y < MAX_COOKIE_Y ; y++) {
+            int currentTag = getTag(x, y);
+            CCNode *node = m_background->getChildByTag(currentTag);
+            if (node && node->boundingBox().containsPoint(touchPoint)) {
+                tag = currentTag;
+                cookieType = ((CookieSprite *) node)->getCookieType();
+                return;
+            }
+        }
+    }
+}
 
+//一致するタグがList中にあればtrue
+bool BattleScene::hasSameColorCookie(std::list<int> cookieTagList, int searchCookieTag) {
+    list<int>::iterator it;
+    for (it = cookieTagList.begin(); it != cookieTagList.end(); ++it) {
+        if (*it == searchCookieTag) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//basetagの上下左右のうち、同じcookieTypeのものをlistにして返す
+list<int> BattleScene::getSameColorCookieTags(int baseTag, kCookie cookieType) {
+    list<int> sameColorCookieTags;
+    sameColorCookieTags.push_back(baseTag);
+
+    list<int>::iterator it = sameColorCookieTags.begin();
+    while (it != sameColorCookieTags.end()) {
+        int tags[] = {
+                *it + 100, // right cookie tag
+                *it - 100, // left cookie tag
+                *it + 1, // up cookie tag
+                *it - 1, // downcookie tag
+        };
+        for (int i = 0; i < sizeof(tags) / sizeof(tags[0]); i++) {
+            if (!hasSameColorCookie(sameColorCookieTags, tags[i])) {
+                if (hasSameColorCookie(m_cookieTags[cookieType], tags[i])) {
+                    sameColorCookieTags.push_back(tags[i]);
+                }
+            }
+        }
+        it++;
+    }
+    return sameColorCookieTags;
+}
+
+//cookieTagsに含まれるすべてのクッキーを削除
+void BattleScene::removeCookie(list<int> cookieTags, kCookie cookieType){
+    list<int>::iterator it = cookieTags.begin();
+    while(it != cookieTags.end()){
+        m_cookieTags[cookieType].remove(*it);
+        CCNode *cookie = m_background->getChildByTag(*it);
+        if(cookie){
+            cookie->removeFromParentAndCleanup(true);
+        }
+        it++;
+    }
+}
+
+
+bool BattleScene::ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent) {
+    return true;
+}
+
+void BattleScene::ccTouchMoved(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent) {
+}
+
+void BattleScene::ccTouchEnded(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent) {
+    CCLog("ccTouchEnded");
+    CCPoint touchPoint = m_background->convertTouchToNodeSpace(pTouch);
+    int tag = 0;
+    kCookie cookieType;
+    getTouchCookieTag(touchPoint, tag, cookieType);
+    if(tag != 0) {
+        list<int> sameColorCookieTags = getSameColorCookieTags(tag, cookieType);
+        if(sameColorCookieTags.size() > 1){
+            removeCookie(sameColorCookieTags, cookieType);
+        }
+    }
+}
